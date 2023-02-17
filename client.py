c839c9cae46f7867760d9b04d716b7aa59d4bc71
StukC <85483301+StukC@users.noreply.github.com>
2023-02-16 23:19:55 -0500

    update client

--- client.py
+++ client.py
@@ -1,29 +1,19 @@
 import socket
 
-SERVER_PORT = 8942
+HOST = 'localhost'
+PORT = 8942
 
-# Create a socket in the Internet domain and connect it to the specified server IP address and port number
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', SERVER_PORT))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        # Prompt user for input
+        user_input = input('Enter command: ')
 
-while True:
-    try:
-        # Send requests to the server via the socket
-        command = input("Enter a command: ")
+        # Send input to server
+        s.sendall(user_input.encode())
 
-        if command == "QUIT":
-            client_socket.sendall(command.encode('utf-8'))
-            break
+        # Receive response from server
+        data = s.recv(1024).decode()
 
-        client_socket.sendall(command.encode('utf-8'))
-
-        # Receive responses from the server via the socket and parse the data
-        response = client_socket.recv(1024).decode('utf-8')
-        print(response)
-
-    except Exception as e:
-        print(f"Error processing request: {str(e)}")
-        break
-
-# Close the socket and exit the program
-client_socket.close()
+        # Print response
+        print('Received:', data.strip())
