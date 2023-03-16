f30d000372dda9f7a9ca3e0c5b695f51330527e7
StukC <85483301+StukC@users.noreply.github.com>
2023-03-15 22:12:17 -0400

    actually working server now

--- server.py
+++ server.py
@@ -1,194 +1,81 @@
 import socket
-import sqlite3
-import sys
+import threading
+from select import select
+import json
+import os
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 8942        # Port to listen on (non-privileged ports are > 1023)
+# Replace with your own file path
+DATA_FILE = "user_data.json"
 
-# Connect to database and create tables if they don't exist
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS Users
-             (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
-             first_name TEXT, 
-             last_name TEXT, 
-             user_name TEXT NOT NULL, 
-             password TEXT,          
-             usd_balance REAL NOT NULL)''')
-c.execute('''CREATE TABLE IF NOT EXISTS Stocks
-             (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
-             stock_symbol TEXT NOT NULL, 
-             stock_name TEXT NOT NULL, 
-             stock_balance REAL, 
-             user_id INTEGER, 
-             FOREIGN KEY(user_id) REFERENCES Users(ID))''')
+def create_sample_data():
+    return {
+        "users": [
+            {
+                "username": "user1",
+                "password": "pass1",
+                "balance": 1000,
+                "stocks": {}
+            },
+            {
+                "username": "user2",
+                "password": "pass2",
+                "balance": 2000,
+                "stocks": {}
+            }
+        ]
+    }
 
-# Insert sample data into Users table
-c.execute("INSERT INTO Users (first_name, last_name, user_name, password, usd_balance) VALUES (?, ?, ?, ?, ?)",
-          ('John', 'Doe', 'jdoe', 'password123', 5000.00))
-c.execute("INSERT INTO Users (first_name, last_name, user_name, password, usd_balance) VALUES (?, ?, ?, ?, ?)",
-          ('Jane', 'Doe', 'jane_doe', 'letmein', 10000.00))
-conn.commit()
+def load_data():
+    if not os.path.exists(DATA_FILE):
+        data = create_sample_data()
+        save_data(data)
+        return data
 
-# Insert sample data into Stocks table
-c.execute("INSERT INTO Stocks (stock_symbol, stock_name, stock_balance, user_id) VALUES (?, ?, ?, ?)",
-          ('AAPL', 'Apple Inc.', 100, 1))
-c.execute("INSERT INTO Stocks (stock_symbol, stock_name, stock_balance, user_id) VALUES (?, ?, ?, ?)",
-          ('MSFT', 'Microsoft Corporation', 50, 2))
-c.execute("INSERT INTO Stocks (stock_symbol, stock_name, stock_balance, user_id) VALUES (?, ?, ?, ?)",
-          ('GOOG', 'Alphabet Inc.', 200, 2))
-conn.commit()
-# Listen for incoming connections
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f'Server listening on {HOST}:{PORT}...')
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024).decode().strip()
-            if not data:
-                break
-            command = data.split()[0].upper()
-            if command == 'BUY':
-                # Parse the arguments and check for errors
-                try:
-                    symbol, amount, price, user_id = data.split()[1:]
-                    amount = float(amount)
-                    price = float(price)
-                    user_id = int(user_id)
-                except ValueError:
-                    conn.sendall(b'Invalid BUY command')
-                    continue
+    with open(DATA_FILE, "r") as f:
+        return json.load(f)
 
-                # Retrieve the user from the database
-                c.execute("SELECT * FROM Users WHERE ID = ?", (user_id,))
-                user = c.fetchone()
-                if not user:
-                    conn.sendall(b'User does not exist')
-                    continue
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f)
 
-                # Calculate the total cost of the purchase and check the user's balance
-                total_cost = amount * price
-                if user[5] < total_cost:
-                    conn.sendall(b'Not enough balance')
-                    continue
+def handle_client(conn, addr, user_data):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        readable, _, _ = select([conn], [], [], 1)
+        if not readable:
+            continue
 
-                # Deduct the cost of the purchase from the user's balance
-                new_balance = user[5] - total_cost
-                c.execute("UPDATE Users SET usd_balance = ? WHERE ID = ?", (new_balance, user_id))
+        data = conn.recv(1024).decode("utf-8")
+        if not data:
+            break
 
-                # Check if the user already owns the stock being purchased
-                c.execute("SELECT * FROM Stocks WHERE user_id = ? AND stock_symbol = ?", (user_id, symbol))
-                stock = c.fetchone()
-                if stock:
-                    # Update the balance of the existing stock
-                    new_stock_balance = stock[3] + amount
-                    c.execute("UPDATE Stocks SET stock_balance = ? WHERE ID = ?", (new_stock_balance, stock[0]))
-                else:
-                    # Add a new stock record for the user
-                    c.execute(
-                        "INSERT INTO Stocks (stock_symbol, stock_name, stock_balance, user_id) VALUES (?, '', ?, ?)",
-                        (symbol, amount, user_id))
+        command, *args = data.split()
+        response = process_command(command, args, user_data)
+        conn.send(response.encode("utf-8"))
 
-                # Commit the changes to the database
-                conn.commit()
+    conn.close()
+    print(f"[DISCONNECTED] {addr} disconnected.")
 
-                # Send the response to the client
-                response = f'200 OK\nBOUGHT: New balance: {amount:.2f} {symbol}. USD balance ${new_balance:.2f}'.encode()
-                conn.sendall(response)
-                pass
-            elif command == 'SELL':
-                # Parse the arguments and check for errors
-                try:
-                    symbol, amount, price, user_id = data.split()[1:]
-                    amount = float(amount)
-                    price = float(price)
-                    user_id = int(user_id)
-                except ValueError:
-                    conn.sendall(b'Invalid SELL command')
-                    continue
+def process_command(command, args, user_data):
+    # Implement command processing logic based on the project requirements.
+    response = f"Received command: {command}, Args: {args}"
+    return response
 
-                # Retrieve the user from the database
-                c.execute("SELECT * FROM Users WHERE ID = ?", (user_id,))
-                user = c.fetchone()
-                if not user:
-                    conn.sendall(b'User does not exist')
-                    continue
+def start():
+    user_data = load_data()  # Load or create user data
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((SERVER, PORT))
+    server.listen()
 
-                # Check if the user owns the stock being sold
-                c.execute("SELECT * FROM Stocks WHERE user_id = ? AND stock_symbol = ?", (user_id, symbol))
-                stock = c.fetchone()
-                if not stock:
-                    conn.sendall(b'User does not own this stock')
-                    continue
+    print(f"[LISTENING] Server is listening on {SERVER}:{PORT}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr, user_data))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
-                # Check if the user has enough stocks to sell
-                if stock[3] < amount:
-                    conn.sendall(b'Not enough stocks to sell')
-                    continue
-
-                # Update the user's USD balance and stock balance
-                total_sale = amount * price
-                new_balance = user[5] + total_sale
-                new_stock_balance = stock[3] - amount
-                c.execute("UPDATE Users SET usd_balance = ? WHERE ID = ?", (new_balance, user_id))
-                c.execute("UPDATE Stocks SET stock_balance = ? WHERE ID = ?", (new_stock_balance, stock[0]))
-
-                # Commit the changes to the database
-                conn.commit()
-
-                # Send the response to the client
-                response = f'200 OK\nSOLD: New balance: {amount:.2f} {symbol}. USD balance ${new_balance:.2f}'.encode()
-                conn.sendall(response)
-                pass
-            elif command == 'BALANCE':
-                args = data.split()[1:]
-                if len(args) != 1:
-                    conn.sendall(b'Invalid BALANCE command')
-                else:
-                    user_id = int(args[0])
-
-                    # Check if user exists
-                    c.execute("SELECT * FROM Users WHERE ID = ?", (user_id,))
-                    user = c.fetchone()
-                    if not user:
-                        conn.sendall(b'User does not exist')
-                        continue
-
-                    # Send response to client
-                    response = f'200 OK\nBalance for user {user[2]} {user[3]}: ${user[5]:.2f}'.encode()
-                    conn.sendall(response)
-                pass
-            elif command == 'LIST':
-                # Retrieve all stocks from the Stocks table
-                c.execute("SELECT * FROM Stocks")
-                stocks = c.fetchall()
-
-                # Generate response string
-                response = ''
-                for stock in stocks:
-                    response += f'{stock[1]} {stock[3]:.1f}\n'
-                response = response.strip().encode()
-
-                # Send response to client
-                conn.sendall(response)
-                pass
-            elif command == 'SHUTDOWN':
-                # Close all open sockets and database connection
-                conn.close()
-
-                sys.exit()
-
-            elif command == 'QUIT':
-                # Send QUIT command to server
-                conn.sendall(b'QUIT\n')
-
-                # Close socket and terminate program
-                s.close()
-                sys.exit()
-                break
-            else:
-                conn.sendall(b'Invalid command')
-        print('Connection closed.')
+if __name__ == "__main__":
+    SERVER = "127.0.0.1"
+    PORT = 5050
+    start()
